c048d96afbe20310454745a32358b790646f684a
Ahmed Osman <Ahmed.Osman.prog@gmail.com>
2023-06-05 19:17:55 +0300

    commit  files

---
+++ Core/Domain.Core/Domain/StrongTypeId.cs
@@ -0,0 +1,46 @@
+﻿namespace Domain.Core.Domain
+{
+    using System.Collections.Generic;
+
+    public abstract class StrongTypeId<TKey> : ValueObject<StrongTypeId<TKey>> where TKey : notnull
+    {
+        protected StrongTypeId(TKey value)
+        {
+            if (value.Equals(Guid.Empty)) throw new ArgumentException("Invalid id");
+            Value = value;
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+        public static bool operator ==(StrongTypeId<TKey>? left, StrongTypeId<TKey>? right)
+        {
+            return EqualOperator(left, right);
+        }
+        public static bool operator !=(StrongTypeId<TKey>? left, StrongTypeId<TKey>? right)
+        {
+            return NotEqualOperator(left, right);
+        }
+        public TKey Value { get; }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TKey>.Default.GetHashCode(Value);
+        }
+    }
+}
---
+++ Services/Money.Wallets/Domain/Wallet.cs
@@ -0,0 +1,42 @@
+﻿namespace Money.Wallets.Domain
+{
+    using global::Domain.Core.Domain;
+    using global::Money.Wallets.Domain.Events;
+
+    public sealed class Wallet: AggregateRoot<WalletId>
+    {
+        private Wallet(string name, string description, Money money)
+        {
+            Name = name;
+            Description = description;
+            Money = money;
+        }
+
+        public string Name { get;private set; }
+        public string Description { get;private set; }
+        public Money Money { get; private set; }
+
+        public static Wallet CreateNew(string name,string description, Money money)
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(name, nameof(name));
+            ArgumentNullException.ThrowIfNullOrEmpty(description, nameof(description));
+            ArgumentNullException.ThrowIfNull(money, nameof(money));
+
+            return new(name, description, money);
+        }
+        public void Update(string name,string description, decimal amount,string currencyCode)
+        {
+            var @event = WalletUpdated.Create(name, description, amount, currencyCode);
+            AppendEvent(@event);
+            Apply(@event);
+        
+        }
+
+        private void Apply(WalletUpdated @event)
+        {
+            Name = @event.Name;
+            Description = @event.Description;
+            Money = Money.Of(@event.Amount, Currency.OfCode(@event.CurrencyCode));
+        }
+    }
+}
